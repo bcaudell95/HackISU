0ee63983f4dfd58f9f65b445dacf7950ad32ea85
bcaudell95 <bcaudell95@gmail.com>
2014-09-13 01:34:45 -0500

    Block texture splits

---
+++ Minecraftify/app/src/main/java/com/hansonsoftware/minecraftify/Color.java
@@ -0,0 +1,17 @@
+package com.hansonsoftware.minecraftify;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Color {
+	public int red, green, blue;
+
+    public Color(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+}
