e9a3137debcf2265fd30000381e5d522f74ef384
Healdo <heealdo@gmail.com>
2014-09-13 10:39:02 -0500

    Mo' camera stuff

--- Minecraftify/app/src/main/java/com/hansonsoftware/minecraftify/PreviewActivity.java
+++ Minecraftify/app/src/main/java/com/hansonsoftware/minecraftify/PreviewActivity.java
@@ -1,14 +1,23 @@
 package com.hansonsoftware.minecraftify;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 /**
  * Created by Adam on 9/13/2014.
  */
 public class PreviewActivity extends Activity {
+    private static final int SELECT_PICTURE = 1;
+    private String selectedImagePath;
+    private ImageView previewPicture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +25,51 @@ public class PreviewActivity extends Activity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_preview);
 
-        previewImage();
+        previewPicture = (ImageView)findViewById(R.id.imageView1);
+        retrieveImage();
     }
 
-    private void previewImage() {
-        
+//    private void previewImage() {
+//        MediaStore.Images.Media preview = new MediaStore.Images.Media();
+//        preview.getContentUri(selectedImagePath);
+//        previewPicture.setImageURI();
+//    }
+
+    private void retrieveImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public String getPath(Uri uri) {
+        // just some safety built in
+        if( uri == null ) {
+            // TODO perform some logging or show user feedback
+            return null;
+        }
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        // this is our fallback here
+        return uri.getPath();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                previewPicture.setImageURI(selectedImageUri);
+                previewPicture.setRotation(90);
+            }
+        }
     }
 }
